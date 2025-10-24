da7842e574e78f2766163f26fdadacceff85dac9
Viral Joshi <5522082+viralj@users.noreply.github.com>
2025-10-24 18:33:20 -0500

    ast: fix Import/FromImport SpanOf signatures to return diag.Span (build fix)

--- compiler/internal/ast/import_node.go
+++ compiler/internal/ast/import_node.go
@@ -2,30 +2,29 @@ package ast
 
 import "github.com/desilang/desi/compiler/internal/diag"
 
-// ImportStmt models:  import dotted.name [as alias]
+// ImportStmt models:   import dotted.name [as alias]
 type ImportStmt struct {
-	// Path is the dotted module path split into segments: ["foo","bar"] for "foo.bar".
-	Path  []string
-	Alias *Ident    // optional local binding name; if nil, last Path segment is bound
-	Span  diag.Span // full statement span
-}
-
-func (*ImportStmt) isStmt()        {}
-func (s *ImportStmt) SpanOf() Span { return s.Span }
-
-// FromImportItem is one imported item in:  from pkg import a [as x], b, ...
-type FromImportItem struct {
-	Name  Ident     // the imported name (value or direct submodule)
-	Alias *Ident    // optional alias
-	Span  diag.Span // span covering the item (name..alias if present)
-}
-
-// FromImportStmt models:  from dotted.name import a [as x], b, ...
-type FromImportStmt struct {
-	Path  []string // dotted module path split into segments
-	Items []FromImportItem
+	Path  []string // dotted path split into segments
+	Alias *Ident   // optional local alias
 	Span  diag.Span
 }
 
-func (*FromImportStmt) isStmt()        {}
-func (s *FromImportStmt) SpanOf() Span { return s.Span }
+func (*ImportStmt) isStmt()             {}
+func (s *ImportStmt) SpanOf() diag.Span { return s.Span }
+
+// FromImportItem is a single item in:   from dotted.name import a [as x], b, ...
+type FromImportItem struct {
+	Name  Ident  // imported item name (identifier)
+	Alias *Ident // optional local alias
+	Span  diag.Span
+}
+
+// FromImportStmt models:   from dotted.name import a [as x], b, ...
+type FromImportStmt struct {
+	Path  []string         // dotted path split into segments
+	Items []FromImportItem // imported items
+	Span  diag.Span
+}
+
+func (*FromImportStmt) isStmt()             {}
+func (s *FromImportStmt) SpanOf() diag.Span { return s.Span }
